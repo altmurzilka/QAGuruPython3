9dab27f7fddde80fb062beeccaab20dd38a9b082
Altyn <altmurzilka@gmail.com>
2023-01-18 22:20:53 +0600

    final commit

--- 2/simple_test.py
+++ 2/simple_test.py
@@ -2,13 +2,11 @@ from selene.support.shared import browser
 from selene import be, have
 
 
-def test_positive():
-    browser.open_url('https://google.com/ncr')
+def test_positive(open_browser):
     browser.element('[name="q"]').should(be.blank).type('selene yashaka').press_enter()
     browser.element('[id="search"]').should(have.text('yashaka/selene: User-oriented Web UI browser'))
 
 
-def test_negative():
-    browser.open_url('https://google.com/ncr')
+def test_negative(open_browser):
     browser.element('[name="q"]').should(be.blank).type('кумпир').press_enter()
     browser.element('[id="search"]').should(have.text('Кумпир, пошаговый рецепт на 520 ккал'))
