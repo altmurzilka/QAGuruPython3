18e5e12aa4c35e6baa368cae324ed28956a25e8a
altmurzilka <altmurzilka@gmail.com>
2023-01-18 14:47:10 +0600

    туц commit

--- 2/main.py
+++ 2/main.py
@@ -1,7 +1,14 @@
-from selene import browser
+from selene.support.shared import browser
 from selene import be, have
 
 
-browser.open_url('https://google.com/ncr')
-browser.element('[name="q"]').should(be.blank).type('selene yashaka').press_enter()
-browser.element('[id="search"]').should(have.text('yashaka/selene: User-oriented Web UI browser'))
+def test_positive():
+    browser.open_url('https://google.com/ncr')
+    browser.element('[name="q"]').should(be.blank).type('selene yashaka').press_enter()
+    browser.element('[id="search"]').should(have.text('yashaka/selene: User-oriented Web UI browser'))
+
+
+def test_negative():
+    browser.open_url('https://google.com/ncr')
+    browser.element('[name="q"]').should(be.blank).type('кумпир').press_enter()
+    browser.element('[id="search"]').should(have.text('Кумпир, пошаговый рецепт на 520 ккал'))
