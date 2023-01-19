fe5c6be47612b361192bf8618e9e4b26e953becf
Altyn <altmurzilka@gmail.com>
2023-01-19 09:44:41 +0600

    заабьюзили

--- 2/simple_test.py
+++ 2/simple_test.py
@@ -8,5 +8,5 @@ def test_positive(open_browser):
 
 
 def test_negative(open_browser):
-    browser.element('[name="q"]').should(be.blank).type('кумпир').press_enter()
-    browser.element('[id="search"]').should(have.text('Кумпир, пошаговый рецепт на 520 ккал'))
+    browser.element('[name="q"]').should(be.blank).type('olololololololoадынадын').press_enter()
+    browser.element('[class="card-section"]').should(have.text('did not match any documents.'))
